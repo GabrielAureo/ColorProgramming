822de3d0b04441806b7bdd81f33fb2c5d1d7a50d
Gabriel Aureo <gabrielaureo@gmail.com>
2024-03-22 06:12:50 -0300

    Splines to edges

--- Assets/ColorProgramming/EdgeController.cs
+++ Assets/ColorProgramming/EdgeController.cs
@@ -12,11 +12,10 @@ namespace ColorProgramming
 
         private LineRenderer lineRenderer;
 
-        [SerializeField]
-        private Material normalMaterial;
+        [SerializeField] private Material normalMaterial;
+        [SerializeField] private Material loopMaterial;
 
-        [SerializeField]
-        private Material loopMaterial;
+        const int numPoints = 10;
 
         void Start()
         {
@@ -27,13 +26,71 @@ namespace ColorProgramming
 
         void Update()
         {
-            UpdateLineRenderer();
+            if (Edge.IsLoop)
+            {
+                RenderBezierLine();
+
+            }
+            else
+            {
+                RenderStraightLine();
+
+            }
+
         }
 
-        private void UpdateLineRenderer()
+        private void RenderStraightLine()
         {
             lineRenderer.SetPosition(0, FromNodeController.transform.position);
             lineRenderer.SetPosition(1, ToNodeController.transform.position);
         }
+
+        private void RenderBezierLine()
+        {
+            // Sample points along the spline
+            Vector3[] splinePoints = new Vector3[numPoints];
+            for (int i = 0; i < numPoints; i++)
+            {
+                float t = i / (float)(numPoints - 1);
+
+                Vector3 v = FromNodeController.transform.position - ToNodeController.transform.position;
+                Vector3 controlPoint = FindPerpendicularVector(v);
+
+
+                splinePoints[i] = CalculateBezierPoint(t, FromNodeController.transform.position, controlPoint, ToNodeController.transform.position);
+            }
+
+            Debug.Log(splinePoints.Length);
+
+            // Set spline points to the LineRenderer
+            lineRenderer.positionCount = numPoints;
+            lineRenderer.SetPositions(splinePoints);
+        }
+
+        private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+        {
+            float u = 1 - t;
+            float tt = t * t;
+            float uu = u * u;
+
+            Vector3 p = uu * p0;
+            p += 2 * u * t * p1;
+            p += tt * p2;
+
+            return p;
+        }
+
+        private Vector3 FindPerpendicularVector(Vector3 originalVector)
+        {
+            Vector3 directionVector = FromNodeController.transform.position - ToNodeController.transform.position;
+            // Find an arbitrary vector not parallel to the original vector
+            Vector3 arbitraryVector = Vector3.up;
+
+            // Cross product to find the perpendicular vector
+            Vector3 perpendicularVector = Vector3.Cross(directionVector.normalized, arbitraryVector).normalized * 3f;
+
+            return perpendicularVector;
+        }
+
     }
 }
