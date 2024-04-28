7af56a4c84d131b26380a77d0edc7c5be8f0410a
Gabriel Aureo <gabrielaureo@gmail.com>
2024-04-28 19:46:29 -0300

    Update select item system

--- Assets/ColorProgramming/GameManager.cs
+++ Assets/ColorProgramming/GameManager.cs
@@ -17,6 +17,7 @@ namespace ColorProgramming
         public NodePrefabCollection NodePrefabCollection;
         public ElementsData ElementsData;
         public StageController StageController;
+        public InventoryController InventoryController;
 
         private void OnEnable()
         {
